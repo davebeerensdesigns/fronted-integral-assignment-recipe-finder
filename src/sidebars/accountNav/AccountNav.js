07d443265a6c76f91048e797a55e9beb4b50181c
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-05-17 17:56:08 +0200

    axios account testing

--- src/sidebars/accountNav/AccountNav.js
+++ src/sidebars/accountNav/AccountNav.js
@@ -1,11 +1,82 @@
 import React, {useState} from 'react';
-import '../../sidebars/accountNav/AccountNav.css';
+import './AccountNav.scss';
 import {faUserPlus, faArrowRightToBracket} from "@fortawesome/pro-regular-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 function AccountNav(props) {
 
     const [accountActiveTab, setAccountActiveTab] = useState('login');
+
+    const [jwtToken, setJwtToken] = useState('');
+
+    function register(){
+        axios.post('https://frontend-educational-backend.herokuapp.com/api/auth/signup', {
+            "username": "jan",
+            "email" : "jan@novi.nl",
+            "password" : "123456",
+            "info" : "dit is het info veld",
+            "role": ["user"]
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function login(){
+        axios.post('https://frontend-educational-backend.herokuapp.com/api/auth/signin', {
+            "username": "jan",
+            "password" : "123456",
+        })
+            .then(function (response) {
+                console.log(response);
+                setJwtToken(response.data.accessToken);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    function getProfile(){
+        axios.get('https://frontend-educational-backend.herokuapp.com/api/user', {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwtToken}`,
+            }
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
+
+    function setProfilePicture(){
+        axios.post('https://frontend-educational-backend.herokuapp.com/api/user/image', {
+            "base64Image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+        },{
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwtToken}`,
+            }})
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+
+
+
+
 
     return (
         <aside
@@ -22,11 +93,17 @@ function AccountNav(props) {
             </nav>
             <div className='tabs'>
                 {accountActiveTab === 'login' &&
-                    'show login'
+                    <button onClick={login}
+                    >login</button>
                 }
                 {accountActiveTab === 'register' &&
-                    'show register'
+                    <button onClick={register}
+                    >register</button>
                 }
+                <button onClick={getProfile}
+                >getProfile</button>
+                <button onClick={setProfilePicture}
+                >setProfileImage</button>
             </div>
         </aside>
     );
