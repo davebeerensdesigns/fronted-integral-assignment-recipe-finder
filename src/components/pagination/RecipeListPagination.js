b58ab449abd500c2740caaad718821b234034918
davebeerensdesigns <84903154+davebeerensdesigns@users.noreply.github.com>
2022-06-16 14:48:22 +0200

    page nav button style

--- src/components/pagination/RecipeListPagination.js
+++ src/components/pagination/RecipeListPagination.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+import  './Pagination.scss';
 
 function RecipeListPagination({offset, number, totalResults, previousLink, previousLabel, nextLink, nextLabel}) {
     return (
         <div className='recipe-list__pagination'>
             {((offset > 0) && (offset - number) >= 0) &&
-                <Link to={previousLink}>{previousLabel}</Link>
+                <Link className='btn btn-sm previous' to={previousLink}>{previousLabel}</Link>
             }
             {((offset + number) < totalResults) &&
-                <Link to={nextLink}>{nextLabel}</Link>
+                <Link className='btn btn-sm next' to={nextLink}>{nextLabel}</Link>
             }
         </div>
     );
